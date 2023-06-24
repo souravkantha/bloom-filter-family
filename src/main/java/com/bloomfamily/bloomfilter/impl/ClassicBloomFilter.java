76b1593e7baf6336eafa8e37fbe93400d150b2c0
Sourav Kantha <souravkantha@Souravs-MacBook-Air.local>
2023-06-24 21:33:44 +0530

    removed main method

--- src/main/java/com/bloomfamily/bloomfilter/impl/ClassicBloomFilter.java
+++ src/main/java/com/bloomfamily/bloomfilter/impl/ClassicBloomFilter.java
@@ -64,38 +64,4 @@ public class ClassicBloomFilter implements BloomFilter {
 
 	}
 	
-public static void main(String[] args) {
-		
-		BloomElement<Integer > element = new BloomElement<>(100);
-		BloomElement<Integer > element1 = new BloomElement<>(101);
-		BloomElement<Integer > element2 = new BloomElement<>(200);
-		BloomElement<Integer > element3 = new BloomElement<>(300);
-		BloomElement<Integer > element4 = new BloomElement<>(300);
-		BloomElement<Integer > element5 = new BloomElement<>(1400);
-		
-		ClassicBloomFilter f = new ClassicBloomFilter(6, 0.1);
-		
-		System.out.println("**");
-//		f.insert(element);
-//		f.insert(element1);
-		f.insert(element2);
-		f.insert(element3);
-		//f.insert(element4);
-		//f.insert(element5);
-		
-		
-		System.out.println(f.probablyHas(element3));
-		
-		System.out.println(f.probablyHas(element5));
-		
-		System.out.println(f.size());
-		
-		//f.delete(element3);
-		
-		System.out.println(f.probablyHas(element3));
-		
-		System.out.println(f.size());
-		
-	}
-	
 }
